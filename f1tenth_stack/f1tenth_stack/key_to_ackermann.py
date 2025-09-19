30c0067fb1d577fd558d1554424f89df5e3ebb84
apric0ts <austinhwang18@gmail.com>
2025-09-18 20:02:26 -0400

    Misc fixes now that servo is up

--- f1tenth_stack/f1tenth_stack/key_to_ackermann.py
+++ f1tenth_stack/f1tenth_stack/key_to_ackermann.py
@@ -42,7 +42,7 @@ class KeyToAckermann(Node):
 
         self.get_logger().info("KeyToAckermann node initialized with velocity scaling.")
 
-        self.timeout = 0.3 
+        self.timeout = 0.05
         self.last_valid_time = time.time()
         self.last_valid_x = 0.0
         self.last_valid_z = 0.0
@@ -64,15 +64,18 @@ class KeyToAckermann(Node):
             self.last_valid_x = x
             self.last_valid_z = z
 
-        scaled_speed = self.linear_scaler.scale(x)
-        scaled_steering = self.angular_scaler.scale(z)
+        # NOTE(austin): invert the speed so that we don't have to swap motor wires
+        scaled_speed = -1 * self.linear_scaler.scale(x)
+        # NOTE(austin): the servo center is offset, we skew it manually
+        skew = - 0.05
+        scaled_steering = skew + self.angular_scaler.scale(z) * 0.25
 
         ackermann_msg = AckermannDriveStamped()
         ackermann_msg.drive.speed = scaled_speed
         ackermann_msg.drive.acceleration = 1.0
         ackermann_msg.drive.jerk = 1.0
         ackermann_msg.drive.steering_angle = scaled_steering
-        ackermann_msg.drive.steering_angle_velocity = 1.0
+        ackermann_msg.drive.steering_angle_velocity = 0.25
 
         self._pub_cmd.publish(ackermann_msg)
 
